31e7841cbd12934b4a9519eec2f7101234f38a2d
unknown <misacinco@gmail.com>
2023-08-29 22:55:56 -0300

    Fix de movimiento de jugador

--- Assets/Scripts/CatScript.cs
+++ Assets/Scripts/CatScript.cs
@@ -4,22 +4,43 @@ using UnityEngine;
 
 public class CatScript : MonoBehaviour
 {
-    public float speed = .1f;
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private float _speed = 5;
+    [SerializeField] private float _turnSpeed = 360;
+    private Vector3 _input;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
+        GatherInput();
+        Look();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        float xDirection = Input.GetAxis("Horizontal");
-        float zDirection = Input.GetAxis("Vertical");
-
-        Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
-
-        transform.position += moveDirection * speed;
+        Move();
     }
+
+    private void GatherInput()
+    {
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    }
+
+    private void Look()
+    {
+        if (_input == Vector3.zero) return;
+
+        var rot = Quaternion.LookRotation(_input.ToIso(), Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, _turnSpeed * Time.deltaTime);
+    }
+
+    private void Move()
+    {
+        _rb.MovePosition(transform.position + transform.forward * _input.normalized.magnitude * _speed * Time.deltaTime);
+    }
+}
+
+public static class Helpers
+{
+    private static Matrix4x4 _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+    public static Vector3 ToIso(this Vector3 input) => _isoMatrix.MultiplyPoint3x4(input);
 }
