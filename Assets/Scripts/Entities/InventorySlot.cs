8a532e14b5ffa47d9e3bc2092685cca78c6b916c
“brenda-mareco” <brendaamareco@gmail.com>
2023-09-16 15:54:48 -0300

    added weapon inventory

---
+++ Assets/Scripts/Entities/InventorySlot.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+public class InventorySlot<T> where T : MonoBehaviour
+{
+    private Transform location;
+    private T element;
+
+    public InventorySlot(Transform location)
+    {
+        this.location = location;    
+    }
+
+    public bool IsFree()
+    { return  element == null; }
+
+    public T GetElement() 
+    { return element; }
+
+    public void SetElement(T element) 
+    {
+        this.element = element;
+        element.transform.parent = location;
+        element.transform.localPosition = Vector3.zero;       
+        element.transform.localRotation = Quaternion.identity;
+    }
+}
--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -5,24 +5,20 @@ using UnityEngine;
 public class Player : DamageableEntity
 {
     [SerializeField] Motion motion;
-    [SerializeField] Weapon defaultWeapon;
-
+        
+    private int m_Experience;
+    private List<InventorySlot<Weapon>> m_WeaponInventory;
     private List<Item> m_Items;
-    private List<Weapon> m_Weapons;
+    private int m_MaxElementsInInventory;
     private PlayerState m_State;
-    public int m_Experience;  
 
     private void Awake()
     {
-        m_Items = new List<Item>();
-
-        defaultWeapon.SetStats(GetStats());
-        SetStats(defaultWeapon);
-
-        m_Weapons = new() { defaultWeapon };
-        m_Experience = 0;
-
+        m_Items = new();
+        m_WeaponInventory = LoadWeaponInventory();
         m_State = new PlayerState(this);
+        m_Experience = 0;
+        m_MaxElementsInInventory = 6;
     }
 
     public void Move(Vector3 vectorMovement)
@@ -32,14 +28,76 @@ public class Player : DamageableEntity
     { m_State = m_State.Attack(target); }
 
     public void Equip(Item item)
-    { m_State = m_State.Equip(item); }
+    { 
+    
+    }
 
     public void Equip(Weapon weapon)
-    { m_State = m_State.Equip(weapon); }
+    { 
+        if (m_WeaponInventory.Count < m_MaxElementsInInventory)
+        {
+            foreach(InventorySlot<Weapon> weaponSlot in m_WeaponInventory)
+            {
+                if (weaponSlot.IsFree())
+                {
+                    weapon.SetStats(GetStats());
+                    SetStats(weapon.GetStats());
+
+                    weapon.gameObject.layer = gameObject.layer;
+                    weaponSlot.SetElement(weapon);
+                }
+            }
+        }
+    }
+
+    public int GetMaxElementsInInventory()
+    { return m_MaxElementsInInventory; }
 
     public List<Weapon> GetWeapons() 
-    { return m_Weapons; }
+    { 
+        List<Weapon> weapons = new();
+
+        foreach(InventorySlot<Weapon> weaponSlot in m_WeaponInventory) 
+        {
+            if (!weaponSlot.IsFree())
+                weapons.Add(weaponSlot.GetElement());
+        }
+
+        return weapons; 
+    }
 
     public List<Item> GetItems() 
     { return m_Items;}
+
+    public int GetExperience()
+    { return m_Experience; }
+
+    public void AddExperience(int experience)
+    { m_Experience += experience; }
+
+
+    public List<InventorySlot<Weapon>> LoadWeaponInventory()
+    {
+        List<InventorySlot<Weapon>> weaponInventory = new();
+        GameObject[] weaponSlots = GameObject.FindGameObjectsWithTag("WeaponSlot");
+
+        for (int i = 0; i < weaponSlots.Length; i++)
+        {
+            InventorySlot<Weapon> slot = new(weaponSlots[i].transform);            
+            Weapon weapon = weaponSlots[i].GetComponentInChildren<Weapon>();
+
+            if (weapon)
+            {
+                weapon.SetStats(GetStats());
+                SetStats(weapon);
+
+                weapon.gameObject.layer = gameObject.layer;
+                slot.SetElement(weapon);
+            }
+
+            weaponInventory.Add(slot);
+        }
+
+        return weaponInventory;
+    }
 }
