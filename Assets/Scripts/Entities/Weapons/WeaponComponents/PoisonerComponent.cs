389767cdf40689aa4f1ba333d1e4206f256ef203
“brenda-mareco” <brendaamareco@gmail.com>
2023-09-26 18:47:52 -0300

    implemented poisoned vfx

--- Assets/Scripts/Entities/Weapons/WeaponComponents/PoisonerComponent.cs
+++ Assets/Scripts/Entities/Weapons/WeaponComponents/PoisonerComponent.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class ConfuserComponent : WeaponComponent
+public class PoisonerComponent : WeaponComponent
 {
+    [SerializeField] float damageAmount;
+
     private bool m_IsCoroutineRunning = false;
 
     public override void HandleOnHit(DamageableEntity damageable) {}
@@ -14,18 +15,21 @@ public class ConfuserComponent : WeaponComponent
         float randomValue = Random.Range(0f, 1f);
 
         if (randomValue < GetProbability() && !m_IsCoroutineRunning)
-            StartCoroutine(nameof(ConfuseCoroutine), player);
+            StartCoroutine(nameof(PoisonCoroutine), player);
         else
             GameEventManager.GetInstance().Publish(GameEvent.WEAPON_COMPONENT_END, new EventContext(this));
     }
 
-    private IEnumerator ConfuseCoroutine(Player player)
+    private IEnumerator PoisonCoroutine(Player player)
     {
+        GameEventManager.GetInstance().Publish(GameEvent.PLAYER_POISONED, new EventContext(player));
         m_IsCoroutineRunning = true;
-
-        player.SetState(new ConfusedPlayerState(player));
-        yield return new WaitForSeconds(GetDuration());
-        player.SetState(new PlayerState(player));
+   
+        for (int i = 0; i < GetDuration(); i++)
+        {
+            player.ReceiveDamage(damageAmount);
+            yield return new WaitForSeconds(1f);
+        }
 
         m_IsCoroutineRunning = false;
         GameEventManager.GetInstance().Publish(GameEvent.WEAPON_COMPONENT_END, new EventContext(this));
