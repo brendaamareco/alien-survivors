7f172c1233e9d68ccdc76d40b37a645b2b36f499
“brenda-mareco” <brendaamareco@gmail.com>
2023-09-26 16:47:57 -0300

    Added lock in stun effect coroutine

--- Assets/Scripts/Entities/Weapons/WeaponComponents/StunnerComponent.cs
+++ Assets/Scripts/Entities/Weapons/WeaponComponents/StunnerComponent.cs
@@ -3,21 +3,26 @@ using UnityEngine;
 
 public class StunnerComponent : WeaponComponent
 {
+    private bool m_IsCoroutineRunning = false;
     public override void HandleOnHit(DamageableEntity damageable) {}
 
     public override void HandleOnHit(Player player)
     {
         float randomValue = Random.Range(0f, 1f);
         
-        if (randomValue < GetProbability())
+        if (randomValue < GetProbability() && !m_IsCoroutineRunning)
             StartCoroutine(nameof(StunCoroutine), player);
     }
 
 
     private IEnumerator StunCoroutine(Player player)
     {
+        m_IsCoroutineRunning = true;
+
         player.SetState(new StunnedPlayerState(player));
         yield return new WaitForSeconds(GetDuration());
         player.SetState(new PlayerState(player));
+
+        m_IsCoroutineRunning = false;
     }
 }
