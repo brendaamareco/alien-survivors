6d88fe8b8c0e6019bde250bc1b54d09a585c2649
unknown <misacinco@gmail.com>
2023-09-30 20:49:49 -0300

    Agregando efecto de blur al momento del game over

--- Assets/Scripts/Controllers/GameOverController.cs
+++ Assets/Scripts/Controllers/GameOverController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
+using UnityEngine.Rendering.PostProcessing;
 
 public class GameOverController : MonoBehaviour
 {
@@ -16,7 +17,10 @@ public class GameOverController : MonoBehaviour
     private VisualElement m_PopUpContainer;
 
     private AudioSource audioTerrain;
-    
+
+    [SerializeField] string postProcessLayerTag = "MainCamera";
+    [SerializeField] LayerMask effectLayer;
+    private PostProcessLayer m_PostProcessLayer;
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +32,9 @@ public class GameOverController : MonoBehaviour
         m_PantallaDerrota.style.height = Length.Percent(100);
         
         audioTerrain = terrain.GetComponent<AudioSource>();
+
+        GameObject go = GameObject.FindGameObjectWithTag(postProcessLayerTag);
+        m_PostProcessLayer = go.GetComponent<PostProcessLayer>();
     }
 
     // Update is called once per frame
@@ -37,6 +44,8 @@ public class GameOverController : MonoBehaviour
     }
     public void Show()
     {
+        m_PostProcessLayer.volumeLayer = effectLayer;
+
         m_PopUpContainer.Add(m_PantallaDerrota);
         m_PopUpContainer.style.display = DisplayStyle.Flex;
 
