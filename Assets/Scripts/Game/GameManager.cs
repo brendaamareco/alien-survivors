5acfc269a1f0e8626080b41619e0a0beb589673f
AgustinMacias <agus_masus@outlook.com>
2023-10-20 15:09:16 -0300

    Timer 2

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -6,16 +7,28 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject UIObject;
     [SerializeField] GameState currentState;
     [SerializeField] float timeLimit;
-    [SerializeField] GameObject boss1;
-    [SerializeField] bool stopSpawn = false;
-    [SerializeField] float spawnTime;
 
     private float stopwatchTime;
     private VisualElement rootStopwatch;
 
+    ///
+
+    [SerializeField] GameObject[] rank1Enemies;
+    [SerializeField] GameObject[] rank2Enemies;
+    [SerializeField] GameObject[] rank3Enemies;
+    [SerializeField] GameObject boss;
+
+    private float spawnTime = 2.0f;    // Initial spawn time
+    private float timer = 0.0f;
+    private float bossSpawnTime = 60.0f;
+    //private float bossSpawnTime = 300.0f;
+    private bool bossDefeated = false;
+    ///
+
     private void Start()
     {
-        Invoke("SpawnObject", spawnTime);
+        Invoke("SpawnBoss", bossSpawnTime);
+        StartCoroutine(SpawnEnemies());
     }
 
     private void Update()
@@ -102,7 +115,7 @@ public class GameManager : MonoBehaviour
         //if (stopwatch.text == "00:10") {Debug.LogWarning("XXXXXXXXXXX");}
     }
 
-    private void SpawnObject()
+    private void SpawnBoss()
     {
         /*
          * Tuve que hacerlo de esta forma sino me tiraba error en la IA de ET:
@@ -110,7 +123,7 @@ public class GameManager : MonoBehaviour
          * desde el editor (hay un checkbox)
          * 2) Activarlo con la línea de código de abajo:
          */
-        boss1.SetActive(true);
+        boss.SetActive(true);
 
 
         //Instantiate(boss1, transform.position, transform.rotation);
@@ -119,4 +132,74 @@ public class GameManager : MonoBehaviour
             CancelInvoke("SpawnObject");
         }*/
     }
+    //////////////////////////////////////////////////////////////////////
+
+    private void SpawnObject(GameObject[] enemies)
+    {
+        if (enemies.Length > 0)
+        {
+            GameObject playerObject = GameObject.FindWithTag("Player");
+            Player player = playerObject.GetComponent<Player>();
+
+            int randomIndex = Random.Range(0, enemies.Length);
+            //Vector3 spawnPosition = player.transform.position + (Random.insideUnitSphere * 5f); // Adjust the radius as needed
+            //Instantiate(enemies[randomIndex], spawnPosition, Quaternion.identity).SetActive(true);
+
+            Vector3 randomDirection = Random.onUnitSphere;
+            randomDirection.y = 0; // Ensure enemies spawn at the same ground level
+            Vector3 spawnPosition = player.transform.position + randomDirection * 15f; // Adjust the radius as needed
+            RaycastHit hit;
+
+            if (Physics.Raycast(spawnPosition + Vector3.up * 10f, Vector3.down, out hit, Mathf.Infinity, LayerMask.GetMask("GroundLayer")))
+            {
+                spawnPosition = hit.point;
+            }
+
+            Instantiate(enemies[randomIndex], spawnPosition, Quaternion.identity).SetActive(true);
+        }
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        while (!bossDefeated)
+        {
+            /*
+            GameObject[] rank1EnemiesClone = new GameObject[rank1Enemies.Length];
+            for (int i = 0; i < rank1Enemies.Length; i++)
+            {
+                GameObject clonedGameObject = Instantiate(rank1Enemies[i]);
+                rank1EnemiesClone[i] = clonedGameObject;
+            }
+            */
+            yield return new WaitForSeconds(spawnTime);
+            SpawnObject(rank1Enemies);
+
+            timer += spawnTime;
+
+            if (timer >= 10.0f)
+            {
+                // After 2 minutes, spawn rank 2 enemies
+                spawnTime = 4.0f;
+                SpawnObject(rank2Enemies);
+            }
+
+            if (timer >= 15.0f)
+            {
+                // After 4 minutes, spawn rank 3 enemies
+                spawnTime = 6.0f;
+                SpawnObject(rank2Enemies);
+                SpawnObject(rank3Enemies);
+            }
+        }
+    }
+
+    public void BossDefeated()
+    {
+        bossDefeated = true;
+        // Stop spawning enemies
+        StopAllCoroutines();
+    }
+
+    //////////////////////////////////////////////////////////////////////
+
 }
