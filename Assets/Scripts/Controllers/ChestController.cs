f8b350f4c53f03f211ac697b6cbcc7506b6fc915
“brenda-mareco” <brendaamareco@gmail.com>
2023-11-27 11:50:39 -0300

    prepared for build

--- Assets/Scripts/Controllers/ChestController.cs
+++ Assets/Scripts/Controllers/ChestController.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
-using static UnityEditor.Progress;
 
 public class ChestController : MonoBehaviour
 {
